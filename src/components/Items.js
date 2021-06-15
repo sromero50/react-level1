8ded2fb4ba68855c83766338e25cae0e2a02accd
RogerMacias <rogermaciasmorilla@gmail.com>
2021-06-15 11:58:13 -0300

    Original

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,9 +1,8 @@
 import './Items.css';
 function Items(props) {
     return (
-        <div className="item">
-            <div>{props.text}</div>
-            <button className="close">&times;</button>
+        <div>
+            
         </div>
     );
 
